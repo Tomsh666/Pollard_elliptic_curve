e637cfe3dbd569b7b2d7403c0be362741f30069b
Tomsh666 <tomshinskiy02@gmail.com>
2024-05-23 00:20:23 +0300

    work: remade to parallel pollard method

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
+from multiprocessing import Process, Manager
 from random import randint
+import pandas as pd
 
 from Crypto.Util.number import inverse
 
 from tools import algebraic_add, scalar_multiply
 
 
-def main():
-    #curve_params, P, Q, q = get_input()
-    curve_params = (1, 11, 307)
+def pollard_parallel(curve_params, P, Q, q, result_dict, proc_id, data_dict):
     p = curve_params[2]
-    P = (306, 304)
-    Q = (146, 65)
-    q = 167
-    d = 0
+    data_list = []
 
-    print(f"\nCurve: y^2 = x^3 + {curve_params[0]}x + {curve_params[1]} (mod {curve_params[2]})")
-    print(f"Point P: {P}")
-    print(f"Point Q: {Q}")
     while True:
         L = randint(1, 50)
         aj = []
@@ -28,9 +22,9 @@ def main():
             tmp_aj = scalar_multiply(aj[j-1], P, curve_params[0], p)
             tmp_bj = scalar_multiply(bj[j-1], Q, curve_params[0], p)
             Rj.append(algebraic_add(tmp_aj, tmp_bj, curve_params[0], p))
-        print(aj)
-        print(bj)
-        print(Rj)
+        print("a_j =", aj)
+        print("b_j =", bj)
+        print("R_j =", Rj)
 
         alpha_1 = randint(0, q - 1)
         betta_1 = randint(0, q - 1)
@@ -40,9 +34,9 @@ def main():
         T_2 = T_1
         alpha_2 = alpha_1
         betta_2 = betta_1
-        print(alpha_2)
-        print(betta_2)
-        print(T_2)
+        print("alpha' =", alpha_1)
+        print("betta' =", betta_1)
+        print("T' =", T_1)
 
         while True:
             # func misc
@@ -60,17 +54,16 @@ def main():
             T_2 = algebraic_add(T_2, Rj[j], curve_params[0], p)
             alpha_2 = (alpha_2 + aj[j]) % q
             betta_2 = (betta_2 + bj[j]) % q
-            print(alpha_1, betta_1, T_1, alpha_2, betta_2, T_2)
+            data_list.append((j, alpha_1, betta_1, T_1, alpha_2, betta_2, T_2))
 
             if T_1 == T_2:
                 break
 
-        if alpha_1 == alpha_2 and betta_1 == betta_2:
-            continue
-        else:
+        if alpha_1 != alpha_2 and betta_1 != betta_2:
             d = (alpha_1 - alpha_2) * inverse(betta_2 - betta_1, q) % q
+            result_dict[proc_id] = d
+            data_dict[proc_id] = data_list
             break
-    print("d =", d, "(mod", q, ")")
 
 
 def H(P, L):
@@ -97,6 +90,38 @@ def get_input():
     return (a, b, p), (Px, Py), (Qx, Qy), q
 
 
+def main():
+    # curve_params, P, Q, q = get_input()
+    curve_params = (1, 11, 307)
+    P = (306, 304)
+    Q = (146, 65)
+    q = 167
+
+    print(f"\nCurve: y^2 = x^3 + {curve_params[0]}x + {curve_params[1]} (mod {curve_params[2]})")
+    print(f"Point P: {P}")
+    print(f"Point Q: {Q}")
+
+    manager = Manager()
+    result_dict = manager.dict()
+    data_dict = manager.dict()
+    processes = []
+
+    for i in range(16):
+        p = Process(target=pollard_parallel, args=(curve_params, P, Q, q, result_dict, i, data_dict))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    for key, value in result_dict.items():
+        print(f"Process {key}: d = {value} (mod {q})")
+
+    for key, data_list in data_dict.items():
+        columns = ['j', 'alpha_1', 'betta_1', 'T_1', 'alpha_2', 'betta_2', 'T_2']
+        df = pd.DataFrame(data_list, columns=columns)
+        print(f"\nData for process {key}:")
+        print(df)
 
 if __name__ == '__main__':
     main()
